9b645e952b4b70f793e6419a8ea964163591adfd
Nathan Clark <nc8774@gmail.com>
2024-10-12 12:19:06 -0400

    fix typo in return type

--- backend/questions.py
+++ backend/questions.py
@@ -1,7 +1,9 @@
 from typing import Literal
 
 question_with_response_type = dict[Literal["question", "response"], str]
-preprompt_type = dict[Literal["user", "assistant"], str]
+preprompt_type = dict[Literal["role", "content"], str]
 
 def convert_questions_to_preprompt(question_response: list[question_with_response_type]) -> list[preprompt_type]:
+    question_response[0]["response"]
+    
     raise NotImplementedError()
