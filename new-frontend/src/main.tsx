e6a0c007e982d3576bb0ea8cc3be2e8e3a72a627
Nathan Clark <nc8774@gmail.com>
2024-10-12 19:45:02 -0400

    demonstrating working API calls with streaming

--- new-frontend/src/main.tsx
+++ new-frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
+import TestEssayEditor from './components/TestEssayEditor.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <TestEssayEditor />
   </StrictMode>,
 )
