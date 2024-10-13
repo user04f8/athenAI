832466004b41d1ad829748e9a74adb6cb89c9eaa
Nathan Clark <nc8774@gmail.com>
2024-10-12 20:31:41 -0400

    adding a proper debugging route

--- new-frontend/src/App.tsx
+++ new-frontend/src/App.tsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Onboarding from './components/Onboarding'
-import EssayEditor from './components/EssayEditor'; // Import the new EssayEditor component
+import EssayEditor from './components/EssayEditor';
+import TestEssayEditor from './components/TestEssayEditor'
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Onboarding />} /> {/* Home route for Orientation */}
         <Route path="/essay-editor" element={<EssayEditor />} /> {/* Route for Essay Editor */}
+
+        <Route path='/test-essay-editor' element={<TestEssayEditor />} /> {/* Purely for debugging */}
       </Routes>
     </Router>
   );
--- new-frontend/src/main.tsx
+++ new-frontend/src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.tsx'
-import TestEssayEditor from './components/TestEssayEditor.tsx'
+import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <TestEssayEditor />
+    <App />
   </StrictMode>,
 )
