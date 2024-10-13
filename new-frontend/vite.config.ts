25d13ca82059ecc03a671f6f0a95348258031b60
Nathan Clark <nc8774@gmail.com>
2024-10-13 00:15:47 -0400

    adding new prod rule

--- new-frontend/vite.config.ts
+++ new-frontend/vite.config.ts
@@ -3,27 +3,33 @@ import react from '@vitejs/plugin-react'
 
 const apiHostname = process.env.VITE_API_HOSTNAME || 'http://localhost:5000';
 
+let proxies = {}
+
+if (apiHostname != 'none') {
+proxies = {
+  '/generate_question': {
+    target: apiHostname,  // Flask server URL
+    changeOrigin: true,
+    secure: false,
+  },
+  '/essay_feedback': {
+    target: apiHostname,  // Flask server URL
+    changeOrigin: true,
+    secure: false,
+  },
+  '/essay_overall_feedback': {
+    target: apiHostname,  // Flask server URL
+    changeOrigin: true,
+    secure: false,
+  },
+  // TODO other API endpoints
+}
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/generate_question': {
-        target: apiHostname,  // Flask server URL
-        changeOrigin: true,
-        secure: false,
-      },
-      '/essay_feedback': {
-        target: apiHostname,  // Flask server URL
-        changeOrigin: true,
-        secure: false,
-      },
-      '/essay_overall_feedback': {
-        target: apiHostname,  // Flask server URL
-        changeOrigin: true,
-        secure: false,
-      },
-      // TODO other API endpoints
-    }
+    proxy: proxies
   }
 })
