a199820ffd88fcf2fdd2ac2aaf0e055f3b05f741
Nathan Clark <nc8774@gmail.com>
2024-10-12 23:58:14 -0400

    test no change proxy

--- new-frontend/vite.config.ts
+++ new-frontend/vite.config.ts
@@ -6,21 +6,21 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/generate_question': {
-        target: 'http://localhost:5000',  // Flask server URL
-        changeOrigin: true,
-        secure: false,
-      },
-      '/essay_feedback': {
-        target: 'http://localhost:5000',  // Flask server URL
-        changeOrigin: true,
-        secure: false,
-      },
-      '/essay_overall_feedback': {
-        target: 'http://localhost:5000',  // Flask server URL
-        changeOrigin: true,
-        secure: false,
-      },
+      // '/generate_question': {
+      //   target: 'http://localhost:5000',  // Flask server URL
+      //   changeOrigin: true,
+      //   secure: false,
+      // },
+      // '/essay_feedback': {
+      //   target: 'http://localhost:5000',  // Flask server URL
+      //   changeOrigin: true,
+      //   secure: false,
+      // },
+      // '/essay_overall_feedback': {
+      //   target: 'http://localhost:5000',  // Flask server URL
+      //   changeOrigin: true,
+      //   secure: false,
+      // },
       // TODO other API endpoints
     }
   }
